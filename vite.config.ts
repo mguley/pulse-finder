8cbc0756c6a37c57dbee703262090a1556d27343
Mykola Guley <mykolaguley99@gmail.com>
2024-08-20 22:33:12 +0300

    Vite configuration changes

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
   build: {
     outDir: '../dist', // This will place the build output in the project root 'dist' folder
     emptyOutDir: true, // Ensures that the output directory is cleared before building
-  }
+  },
+  base: '/pulse-finder/', // Set the base path for GitHub Pages
 });
